d84a32f85298e68dac557b5cfb2c651c1d4be2e3
Moein Samani Nejad <moeinsamani1992@gmail.com>
2025-03-12 08:12:15 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  
+  return (
+    <div className="text-center">
+      <h1>Hello Moein Samani Nejad</h1>
+      <p>You are the greatest and I belive in your Power!</p>
+    </div>
+  );
+};
+
+export default App;
